ef0ec12bd3e6554de490d1b05930e19847acf6a0
Jacky Le <lenghia1991@gmail.com>
2022-07-03 10:11:01 +0700

    add function to handle command (try with g, tw, ig, fb, gh)

--- src/controller/searchController.ts
+++ src/controller/searchController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
 import {
-  getCommandFromQueryString,
   constructGoogleSearchUrl,
+  constructNormalUrl,
+  getCommandFromQueryString,
+  getSearchQueryFromQueryString,
 } from "../utils/index";
+
+const defaultCommand = [
+  { command: "g", redirect: "https://google.com/search?q=" },
+  { command: "tw", redirect: "https://twitter.com" },
+  { command: "ig", redirect: "https://instagram.com" },
+  { command: "fb", redirect: "https://facebook.com" },
+  { command: "gh", redirect: "https://github.com" },
+];
 
 export const getAll = async (req: Request, res: Response) => {
   const { cmd } = req.query;
@@ -16,13 +26,19 @@ export const getAll = async (req: Request, res: Response) => {
     return;
   }
 
+  const commandList = new Map();
+  defaultCommand.map((command) => {
+    commandList.set(command.command, command.redirect);
+  });
   const command = getCommandFromQueryString(cmd);
-  switch (command) {
-    case "tw":
-      res.redirect("https://twitter.com");
-      break;
-    default:
-      res.redirect(constructGoogleSearchUrl(command));
-      break;
+  const foundCommand = commandList.get(command);
+  if (foundCommand) {
+    res.redirect(
+      constructNormalUrl(foundCommand) + getSearchQueryFromQueryString(cmd)
+    );
+    return;
+  } else {
+    res.redirect(constructGoogleSearchUrl(cmd));
+    return;
   }
 };
--- src/utils/queryStringUtils.ts
+++ src/utils/queryStringUtils.ts
@@ -2,8 +2,19 @@ export const getCommandFromQueryString = (query: string): string => {
   return query.split(" ")[0];
 };
 
+export const getSearchQueryFromQueryString = (query: string): string => {
+  const cmd = getCommandFromQueryString(query);
+
+  return query.substring(cmd.length).trim();
+};
+
+export const constructNormalUrl = (query: string): string => {
+  //TODO: enchance regex instead of 2 matches
+  return (query.match(/\/$/) || query.match(/\?/)) ? query : query + "/";
+};
+
 export const constructGoogleSearchUrl = (query: string): string => {
   const googleString = "https://google.com/search?q=";
 
-  return googleString + encodeURI(query);
+  return googleString + encodeURI(query.trim());
 };
