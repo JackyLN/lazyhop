1dd2e5102ecaddbd191bd7cee2e24b7d1ed3db52
Jacky Le <lenghia1991@gmail.com>
2022-07-03 08:14:14 +0700

    Get command and build default search redirect to search in google

--- src/controller/searchController.ts
+++ src/controller/searchController.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from "express";
+import { getCommandFromQueryString, constructGoogleSearchUrl } from "../utils/index";
 
 export const getAll = async (req: Request, res: Response) => {
   const { cmd } = req.query;
-  
-  switch (cmd) {
+
+  if (!cmd) {
+    res.status(404);
+    return;
+  }
+  if (typeof cmd !== "string") {
+    res.status(500);
+    return;
+  }
+
+  let command = getCommandFromQueryString(cmd);
+  console.log(command);
+  switch (command) {
     case "tw":
       res.redirect("https://twitter.com");
       break;
     default:
-      res.redirect("https://google.com");
+      res.redirect(constructGoogleSearchUrl(command));
       break;
   }
 };
--- src/utils/queryStringUtils.ts
+++ src/utils/queryStringUtils.ts
@@ -1,3 +1,9 @@
-export const getCommandFromQueryString = (): String => {
-  return "";
+export const getCommandFromQueryString = (query: string): string => {
+  return query.split(" ")[0];
+};
+
+export const constructGoogleSearchUrl = (query: string): string => {
+  const googleString = "https://google.com/search?q=";
+
+  return googleString + encodeURI(query);
 };
